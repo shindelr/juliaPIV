c7fb74483f87a7e9f8d9aada3889069287c76a14
shindelr <129821483+shindelr@users.noreply.github.com>
2025-06-30 12:03:45 -0700

    Ironed out more pypi upload issues and put version 0.0.10 up on PyPi proper

--- src/PIVPipelineUtility/precompile.jl
+++ src/PIVPipelineUtility/precompile.jl
@@ -5,8 +5,6 @@ Base.ARGS = ["2",
 "24,2425,1,2048", 
 "16", 
 "0.5",
-# "../tests/pipeline_utility_testing/SVSout_23227179_1724441851/precompile_pivframes",
-# "../tests/pipeline_utility_testing/testbatches/tmp.2YNbHiPCwK.txt", 
 "/Users/robinshindelman/repos/Nearshore-Research/tests/abs-batch-testing/piv_out/",
 "/Users/robinshindelman/repos/Nearshore-Research/tests/abs-batch-testing/batches/batchtest.txt",
  "1"]
